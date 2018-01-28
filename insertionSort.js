3b998e1f1b8e8e3796c025562515cae89a223666
annabush092 <annabush092@gmail.com>
2018-01-28 14:48:23 -0500

    found diff between bubble and insertion

--- insertionSort.js
+++ insertionSort.js
@@ -1,7 +1,12 @@
+// O(n^2)
+
+// Bubble sort: does not move the element all the way to it's proper place, it just switches and moves on, and then repeats the entire process until they're all sorted.
+// So difference between Insertion Sort and Bubble Sort is that Insertion sort, after switching two, will go back and index and check if the just-moved element needs to be switched again. Bubble sort does not do this, it just buldozes through every pair in the array and then starts over from the beginning.
+
 function insertionSort(arr) {
   //go through the array in pairs until you find a number smaller than the one before it
   //switch it with the number before it
-  //go back an index, check max/min, switch it if it is the wrong order
+  //go back an index, check max/min, switch it if it is the wrong order (this step is skipped in bubble sort)
   //continue until it's the right order
   //start back where you left off, find the next num out of order
   for(let i=1; i < arr.length; i++){
--- selectionSort.js
+++ selectionSort.js
@@ -1,7 +1,9 @@
+// O(n^2)
+
 function selectionSort(arr) {
-  // find minimum of remaining nums
-  //put minimum at beginning
-  //repeat from next index
+  // find minimum of the array (the first time around this is the entire array)
+  //select minimum and swap with the first element of the array
+  //repeat for a new smaller array that starts from next index
   for(let j=0; j<arr.length; j++) {
     let minIndex = j
     for(let i= minIndex + 1; i<arr.length; i++) {
